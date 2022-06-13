ad86df49bb1daa9cc9daa6650ddba337cc812400
Roger Esteve <rogeresteve30@gmail.com>
2022-06-13 21:58:10 +0200

    mute funciona

--- js/app.js
+++ js/app.js
@@ -7,8 +7,19 @@ $(document).ready(function() {
     joc.velocitat=1;
     joc.inicialitza();
   
-
+    $('#check_mute').change(function () {
+        const muted = $('#check_mute').is(':checked');
+        setMute(muted);
+    });
 });
+
+function setMute(val) { 
+    const elems = document.querySelectorAll("video, audio");
+    for (const el of elems) {
+        el.muted = val;
+        el.pause();
+    }
+}
 
 function animacio() {
     joc.update();
