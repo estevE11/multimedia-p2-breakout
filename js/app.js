2493d72d90d6505f5193f7eb3d5901b2959fb559
Roger Esteve <rogeresteve30@gmail.com>
2022-06-16 15:04:55 +0200

    top 3 puntuacions

--- js/app.js
+++ js/app.js
@@ -6,7 +6,9 @@ $(document).ready(function () {
     joc = new Joc(myCanvas, ctx);
     joc.velocitat = 1;
     ///joc.inicialitza();
-  
+
+    updateRanking();
+
     $('#check_mute').prop('checked', true);
     $('#check_mute').change(function () {
         const muted = $('#check_mute').is(':checked');
@@ -27,6 +29,35 @@ $(document).ready(function () {
         $('#mainmenu').show();
     });
 });
+
+function updateRanking() {
+    let orderedScores = [];
+    let scores = {};
+    let val = localStorage['scores'];
+
+    if (val) {
+        scores = JSON.parse(val);
+    }
+
+    if (Object.keys(scores).length > 0) {
+        console.log('caca');
+        while (orderedScores.length < 3 && Object.keys(scores).length > 0) {
+            const keys = Object.keys(scores);
+            let hi = keys[0];
+            for (let i = 1; i < keys.length; i++) {
+                if (scores[keys[i]] > scores[hi]) {
+                    hi = keys[i];
+                }
+            }
+            orderedScores.push(hi + ' - ' + scores[hi] + 'pts');
+            delete scores[hi];
+        }
+    }
+
+    orderedScores.forEach(function (it, i) {
+        $('#ranking' + (i + 1)).text(it);
+    });
+}
 
 function setMute(val) { 
     const elems = document.querySelectorAll("video, audio");
